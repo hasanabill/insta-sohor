7d44a903f4bf7e8f94d3b15ad79e02137de74fc5
hasanabil <mahmudul.anabil2862@gmail.com>
2022-03-10 21:56:53 +0600

    reported post fixed

--- js/app.js
+++ js/app.js
@@ -43,6 +43,7 @@ const switchTab = (id) => {
 
         displayLikedPosts();
     } else {
+        document.getElementById( "reported" ).textContent = "";
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
@@ -154,7 +155,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
